e8e2cc0750e0c7710028c81004d165fae63a79ce
G13Dantas <gdantasestudos01@gmail.com>
2025-06-09 14:27:31 -0300

    me

---
+++ AGENDA 04/Prativa01.java
@@ -0,0 +1,9 @@
+Public class Principal{
+
+    public static void(String[]args){
+        
+        meuprimeiroprograma="Ola mundo"
+        
+        System.out.println(meuprimeiroprograma);
+    }
+}
