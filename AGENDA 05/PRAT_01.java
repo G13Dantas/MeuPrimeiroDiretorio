e47058a8b76174d456417ce71627b55455468732
G13Dantas <gdantasestudos01@gmail.com>
2025-06-09 21:20:24 -0300

    me

--- AGENDA 05/PRAT_01.java
+++ AGENDA 05/PRAT_01.java
@@ -1,5 +1,13 @@
 public class Principal{
     public static void main(String[] args){
-        
+        int idade=20;
+        double altura=1.75;
+        char inicial='Amora';
+        String nome="Gilberto Gil";
+        boolean Aprovado=true;
+
+        System.out.println("Idade: "+idade);
+        System.out.println("Altura: "+altura);
+        System.out.println("Inicial: "+inicial);
     }
 }
