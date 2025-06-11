3d6decc0485e4d9f16d8e85542c3660c85b358da
Mr Dnts <gdantasestudos01@gmail.com>
2025-06-11 20:39:22 -0300

    me

--- AGENDA 05/PJ03.java
+++ AGENDA 05/PJ03.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+// Comando para importar a biblioteca Scanner, que permite a leitura de dados do usuário.
+
 public class PJ03 {
     public static void main(String[] args){
+
+        System.out.println((3+(7+3))*2);
+        // Exibe o resultado da expressão (3 + (7 + 3)) * 2 na tela.
+
+        System.out.println(168/46);
+        // Exibe o resultado da divisão de 168 por 46 na tela.
+
+        System.out.println((7%4)*2);
+        // Exibe o resultado da expressão (7 % 4) * 2 na tela, onde % é o operador de módulo que retorna o resto da divisão de 7 por 4.
+
+        System.out.println("Técnico" + " Módulo "+ "1");
+        // Exibe a concatenação das strings "Técnico", " Módulo " e "1" na tela.
         
     }
     
