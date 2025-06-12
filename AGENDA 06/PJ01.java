c469e5fa9aef8bcaddc2a2ff6f06aeda6fe4f74a
Mr Dnts <gdantasestudos01@gmail.com>
2025-06-11 21:47:03 -0300

    me

---
+++ AGENDA 06/PJ01.java
@@ -0,0 +1,18 @@
+// Agenda focada em estruturas de decisao //
+import java.util.Scanner;
+
+public class PJ01 {
+    public static void main(String[] args){
+
+        int idade; //armazena a idade//
+
+        Scanner leitor = new Scanner(System.in); //Criação objeto "leitor" para inserir dados//
+
+        System.out.println("Digite sua idade: ");
+        idade = leitor.nextInt(); //Lê a idade digitada pelo usuário e armazena na variável idade//
+
+        if (idade >= 18) { //verifica se a idade é maior que 18//
+            System.out.println("Você é maior de idade!");
+        }
+    }
+}
